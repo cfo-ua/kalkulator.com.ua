d1e06dadeec889dfb217747acf91e5de08b25834
cfo-ua <cfo.ua01@gmail.com>
2025-05-30 07:36:44 +0200

    Update loan.js

--- assets/js/loan.js
+++ assets/js/loan.js
@@ -13,9 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Annuity formula
     const payment = (S * r) / (1 - Math.pow(1 + r, -n));
     const total = payment * n;
+    // Format numbers for Ukrainian locale
+    const paymentFormatted = payment.toLocaleString('uk-UA', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    const totalFormatted = total.toLocaleString('uk-UA', {minimumFractionDigits: 2, maximumFractionDigits: 2});
     result.innerHTML = `
-      <b>Щомісячний платіж:</b> ${payment.toFixed(2)} ₴<br>
-      <b>Загальна сума виплат:</b> ${total.toFixed(2)} ₴
+      <b>Щомісячний платіж:</b> ${paymentFormatted} ₴<br>
+      <b>Загальна сума виплат:</b> ${totalFormatted} ₴
     `;
   });
 });
